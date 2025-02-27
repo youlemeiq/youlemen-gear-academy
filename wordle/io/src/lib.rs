bdddaa882f224e4118a419b5ed207eb5e94626b8
youlemeiq <youlemeiq@outlook.com>
2025-02-27 15:18:42 +0800

    Add files via upload

---
+++ wordle/io/src/lib.rs
@@ -0,0 +1,24 @@
+#![no_std]
+use gstd::{prelude::*, ActorId, Decode, Encode, TypeInfo};
+
+#[derive(Debug, Clone, Encode, Decode, TypeInfo, PartialEq, Eq)]
+#[codec(crate = gstd::codec)]
+#[scale_info(crate = gstd::scale_info)]
+pub enum WordleAction {
+    StartGame { user: ActorId },
+    CheckWord { user: ActorId, word: String },
+}
+
+#[derive(Debug, Clone, Encode, Decode, TypeInfo, PartialEq, Eq)]
+#[codec(crate = gstd::codec)]
+#[scale_info(crate = gstd::scale_info)]
+pub enum WordleEvent {
+    GameStarted {
+        user: ActorId,
+    },
+    WordChecked {
+        user: ActorId,
+        correct_positions: Vec<u8>,
+        contained_in_word: Vec<u8>,
+    },
+}
